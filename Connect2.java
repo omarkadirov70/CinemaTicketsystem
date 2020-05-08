3a84be934f364751459e41dc65ffdc15b045e2a0
Omar <omar@gmail.com>
2020-05-08 23:51:04 +0600

    123

---
+++ Connect2.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Connect2{
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet resultSet = null;
+    private static String url = "jdbc:mysql://localhost:3306/sales?serverTimezone=Europe/Moscow&useSSL=false";
+    private static String username = "root";
+    private static String password = "";
+
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        }
+        catch(Exception e){
+            System.out.println("Connection failed...");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
